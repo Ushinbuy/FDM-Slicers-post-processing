7978173a799093bb4130a4d0934367974d759b19
Ushinbuy <n.s.shadrin@gmail.com>
2022-08-22 01:09:42 +0300

    remove unused comments

--- LA.sh
+++ LA.sh
@@ -12,16 +12,10 @@ exec 2>&1
 echo "Arguments is"
 echo "${@}"
 
-ARG1=$1
-ARG2=$2
-
-# /usr/bin/env python3 $DIR/LAsimplify3D.py "${@}"
-
-# switch file and hwconfig argument order
-if [ "$ARG2" == "-s3d" ]; then
+if [ "$2" == "-s3d" ]; then
   echo "start script"
   python3 $DIR/linearAdvance.py "${@}"
-elif [ "$ARG1" == "-ps" ]; then
+elif [ "$1" == "-ps" ]; then
   echo "start script"
   python3 $DIR/linearAdvance.py "$2" "$1"
 else
